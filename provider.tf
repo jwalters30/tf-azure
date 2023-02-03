96c67bc2f26d3e094654f541fac07b2e192af771
Jim Walters <jwalters@go-planet.com>
2023-02-03 14:09:04 -0600

    initial commit

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">=1.2"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.40"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-rg"
+    storage_account_name = "jwalterstfstorage202302"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+#    use_oidc = true
+  }
+}
+
+provider "azurerm" {
+#  use_oidc = true
+  features {}
+}
