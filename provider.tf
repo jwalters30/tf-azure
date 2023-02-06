7a12faf1ecb182fed625d27816d7e767a708e6e1
Jim Walters <jwalters@go-planet.com>
2023-02-06 13:12:59 -0600

    formatting

--- provider.tf
+++ provider.tf
@@ -12,11 +12,11 @@ terraform {
     storage_account_name = "jwalterstfstorage202302"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-#    use_oidc = true
+    #    use_oidc = true
   }
 }
 
 provider "azurerm" {
-#  use_oidc = true
+  #  use_oidc = true
   features {}
 }
