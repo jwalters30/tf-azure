ca6350352c72dd4d94e45146ce40e5deb6fb1c61
jwalters30 <jwalters@quad-c.com>
2023-02-07 14:37:08 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,8 +28,39 @@ module "enterprise_scale" {
     azurerm.management   = azurerm
   }
 
+  # Base module configuration settings
   root_parent_id = data.azurerm_client_config.core.tenant_id
-  root_id        = "jimorg"
-  root_name      = "Jim Organization"
+  root_id        = var.root_id
+  root_name      = var.root_name
+  library_path   = "${path.module}/lib"
 
+  # Enable creation of the core management group hierarchy
+  # and additional custom_landing_zones
+  deploy_core_landing_zones = true
+  custom_landing_zones      = local.custom_landing_zones
+
+  # Configuration settings for identity resources is
+  # bundled with core as no resources are actually created
+  # for the identity subscription
+  deploy_identity_resources    = true
+  configure_identity_resources = local.configure_identity_resources
+  subscription_id_identity     = var.subscription_id_identity
+
+  # The following inputs ensure that managed parameters are
+  # configured correctly for policies relating to connectivity
+  # resources created by the connectivity module instance and
+  # to map the subscription to the correct management group,
+  # but no resources are created by this module instance
+  deploy_connectivity_resources    = false
+  configure_connectivity_resources = data.terraform_remote_state.connectivity.outputs.configuration
+  subscription_id_connectivity     = data.terraform_remote_state.connectivity.outputs.subscription_id
+
+  # The following inputs ensure that managed parameters are
+  # configured correctly for policies relating to management
+  # resources created by the management module instance and
+  # to map the subscription to the correct management group,
+  # but no resources are created by this module instance
+  deploy_management_resources    = false
+  configure_management_resources = data.terraform_remote_state.management.outputs.configuration
+  subscription_id_management     = data.terraform_remote_state.management.outputs.subscription_id
 }
