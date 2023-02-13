c31eb4338707d1a8e5c5176b40c24a176423f85e
Jim Walters <jwalters@go-planet.com>
2023-02-13 11:59:17 -0600

    adding MGs back in, fixing missing lockfile exclusion

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-/* resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "westus2"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
 # You can use the azurerm_client_config data resource to dynamically
 # extract connection settings from the provider configuration.
 
@@ -17,11 +5,9 @@ data "azurerm_client_config" "core" {}
 
 # Call the caf-enterprise-scale module directly from the Terraform Registry
 # pinning to the latest version
-
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
   version = "3.1.2"
-
   providers = {
     azurerm              = azurerm
     azurerm.connectivity = azurerm
@@ -34,18 +20,40 @@ module "enterprise_scale" {
   root_name      = var.root_name
   library_path   = "${path.module}/lib"
 
+  custom_landing_zones = {
+    "${var.root_id}-Hub" = {
+      display_name               = "${upper(var.root_id)} Hub"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "${var.root_id}-Spokes" = {
+      display_name               = "${upper(var.root_id)} Spokes"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+  }
+
+  /*
   # Enable creation of the core management group hierarchy
   # and additional custom_landing_zones
   deploy_core_landing_zones = true
   custom_landing_zones      = local.custom_landing_zones
-
   # Configuration settings for identity resources is
   # bundled with core as no resources are actually created
   # for the identity subscription
   deploy_identity_resources    = true
   configure_identity_resources = local.configure_identity_resources
   subscription_id_identity     = var.subscription_id_identity
-
   # The following inputs ensure that managed parameters are
   # configured correctly for policies relating to connectivity
   # resources created by the connectivity module instance and
@@ -54,7 +62,6 @@ module "enterprise_scale" {
   deploy_connectivity_resources    = false
   configure_connectivity_resources = data.terraform_remote_state.connectivity.outputs.configuration
   subscription_id_connectivity     = data.terraform_remote_state.connectivity.outputs.subscription_id
-
   # The following inputs ensure that managed parameters are
   # configured correctly for policies relating to management
   # resources created by the management module instance and
@@ -63,9 +70,9 @@ module "enterprise_scale" {
   deploy_management_resources    = false
   configure_management_resources = data.terraform_remote_state.management.outputs.configuration
   subscription_id_management     = data.terraform_remote_state.management.outputs.subscription_id
+*/
 }
 
-*/
 variable "resource_group_name" {
   type    = list(string)
   default = ["rg-test-1", "rg-test-2"]
