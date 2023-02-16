125eb68ed37a87e185fbc548257962c6685e05e7
jwalters30 <jwalters@quad-c.com>
2023-02-16 08:33:40 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,6 +107,20 @@ resource "azurerm_resource_group" "test" {
   location = "eastus"
 }
 
+resource "azurerm_virtual_network" "test" {
+  name = "JimVnet"
+  resource_group_name = "rg-test-1"
+  location = "eastus"
+  address_space = "10.0.0.0/0"
+}
+
+resource "azurerm_subnet" "snet" {
+  name                 = "subnet1"
+  resource_group_name  = "rg-test-1"
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = "10.0.0.0/24"
+}
+
 module "service-principal" {
   source  = "kumarvna/service-principal/azuread"
   version = "2.3.0"
