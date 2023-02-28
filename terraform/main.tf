edb66ea4a6f2902f4a737dd010a2f7ce4009bd2f
jwalters30 <jwalters@quad-c.com>
2023-02-28 09:23:57 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,7 +118,7 @@ resource "azurerm_subnet" "snet" {
   name                 = "subnet1"
   resource_group_name  = "rg-test-1"
   virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = [ "10.0.1.0/24" ]
+  address_prefixes     = [ "10.0.0.0/24" ]
 }
 
 module "service-principal" {
